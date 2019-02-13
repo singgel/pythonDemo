acc265c6825a05d0360495fc3e6fd6904c866137
heks <heks@yiche.com>
2019-02-13 18:42:12 +0800

    redis集群操作

---
+++ redisCluster/removeRedisKey.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+
+from rediscluster import StrictRedisCluster
+import sys
+
+
+def redis_cluster():
+    redis_nodes = [{'host': '172.20.4.91', 'port': 7000},
+                   {'host': '172.20.4.91', 'port': 7001},
+                   {'host': '172.20.4.92', 'port': 7002},
+                   {'host': '172.20.4.92', 'port': 7003},
+                   {'host': '172.20.4.95', 'port': 7004},
+                   {'host': '172.20.4.95', 'port': 7005}]
+    try:
+        redisconn = StrictRedisCluster(startup_nodes=redis_nodes)
+    except Exception:
+        print("Connect Error!")
+        sys.exit(1)
+
+    redisconn.set('name', 'admin')
+    print("name is: ", redisconn.get('name'))
+
+redis_cluster()
